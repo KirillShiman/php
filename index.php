ba3395870c043dbb28e23d3fa031675261294736
KirllShiman <shiman.2004@bk.ru>
2025-01-26 21:13:34 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$helloLine="Hello World   ";
+$inc=10;
+$incj=10;
+for($i=0;$i<$inc;$i++){
+    for($j=0;$j<$incj;$j++){
+        echo $helloLine;
+
+    }
+    echo "<br>";
+    $incj--;
+}# php
